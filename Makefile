d2cb9cbae0df2476971c05f59d4f8017bc998606
foway0 <foway0@gmail.com>
2021-10-08 01:24:09 +0900

    make init

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 include scripts/variables.mk
 
 test: ## check dependencies
+	command -v npm
 	command -v docker
 init: ## init repos
-	@sh ${INIT} -t ${REPOS_CONF} -o ${OUTPUT_PATH}
+	@sh ${INIT} -c init -t ${REPOS_CONF} -o ${OUTPUT_PATH}
+init-clean: ## clean init repos
+	@sh ${INIT} -c clean -t ${REPOS_CONF} -o ${OUTPUT_PATH}
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-while getopts t:o: options
+while getopts c:t:o: options
 do
     case "${options}" in
+        c) COMMAND=${OPTARG};;
         t) TARGET=${OPTARG};;
         o) OUTPUT=${OPTARG};;
     esac
 done
 
-mkdir -p $OUTPUT
+case $COMMAND in
+  "init")
+    mkdir -p $OUTPUT
 
-pushd $OUTPUT >/dev/null 2>&1 || exit
-while read -r line;
-do
-  echo "$line"
-  url=$(echo "$line" | cut -d' ' -f1)
-  branch=$(echo "$line" | cut -d' ' -f2)
-  name=$(echo "$line" | cut -d' ' -f3)
-  if [ -d "./${name}" ]
-  then
-    echo "SKIP... ${name} directory already exists"
-  else
-    echo "clone repositories"
+    pushd $OUTPUT >/dev/null 2>&1 || exit
+    while read -r line;
+    do
+      echo "$line"
+      url=$(echo "$line" | cut -d' ' -f1)
+      branch=$(echo "$line" | cut -d' ' -f2)
+      name=$(echo "$line" | cut -d' ' -f3)
+      if [ -d "./${name}" ]
+      then
+        echo "SKIP... ${name} directory already exists"
+      else
+        echo "clone repositories"
 
-    git clone ${url} ${name}
+        git clone ${url} ${name}
 
-    git checkout ${branch}
-  fi
-done < $TARGET
-popd $OUTPUT >/dev/null 2>&1 || exit
+        git checkout ${branch}
+      fi
+    done < $TARGET
+    popd $OUTPUT >/dev/null 2>&1 || exit
+    ;;
+  "clean")
+    rm -rf $OUTPUT
+    ;;
+  *)
+    echo "???";;
+esac
